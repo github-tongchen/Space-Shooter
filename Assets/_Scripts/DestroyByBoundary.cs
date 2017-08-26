e3c951d5e1089644e1422d411c3c6914d7ce2adf
tongchen <839706407@qq.com>
2017-08-26 09:34:39 +0800

    完成子弹的发射，设置触发器销毁超出边界的子弹

---
+++ Assets/_Scripts/DestroyByBoundary.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByBoundary : MonoBehaviour
+{
+
+    //  当其他碰撞体超出触发器的容量时销毁碰撞体
+    void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -13,6 +13,23 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float tilt;
     public Boundary boundary;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    //  子弹发射的间隔，也就是子弹发射的时间间隔
+    public float fireRate;
+    //  下一次发射的时间
+    private float nextFire;
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+    }
+
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
@@ -25,9 +42,9 @@ public class PlayerController : MonoBehaviour
         //  控制飞船不会飞出屏幕
         rigidbody.position = new Vector3
         (
-        Mathf.Clamp(rigidbody.position.x,boundary.xMin,boundary.xMax),
+        Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
         0.0f,
-        Mathf.Clamp(rigidbody.position.z,boundary.zMin,boundary.zMax)
+        Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
         );
         //  飞船移动时倾斜一点
         rigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, -rigidbody.velocity.x * tilt);

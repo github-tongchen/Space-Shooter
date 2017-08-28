3c862d5ba2163b1b648350411ea81512a0d91580
tongchen <839706407@qq.com>
2017-08-28 18:11:57 +0800

    修复

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -24,16 +24,29 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         //  检测左Ctrl键
-        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        /*if (Input.GetButton("Fire1") && Time.time > nextFire)
         {
             nextFire = Time.time + fireRate;
             Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
             GetComponent<AudioSource>().Play();
+        }*/
+
+        if (Input.touchCount > 0)//如果存在Touch事件  
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)//相当于TouchDown  
+            {
+                nextFire = Time.time + fireRate;
+                Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+                GetComponent<AudioSource>().Play();
+            }
         }
+        
     }
 
     void FixedUpdate()
     {
+        //  键盘方向键控制
+        /*
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
@@ -48,7 +61,21 @@ public class PlayerController : MonoBehaviour
         0.0f,
         Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
         );
+
         //  飞船移动时倾斜一点
         rigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, -rigidbody.velocity.x * tilt);
+        */
+
+        //  跟随鼠标
+        gameObject.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 10));
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        //  控制飞船不会飞出屏幕
+        rigidbody.position = new Vector3
+        (
+        Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+        0.0f,
+        Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
+        );
     }
 }
